082de52cba3af526dc8fd1c94173234d6b63f6f8
itskundan-01 <2300030371@kluniversity.in>
2025-01-18 23:26:10 +0530

    added svg file and made some changes related to server for backend on aws

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,29 @@
-require('dotenv').config({ path: './.env' });
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
+// Debugging: Verify if MONGO_URI is loaded
+console.log('MONGO_URI:', process.env.MONGO_URI);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://todo-app.kundanprojects.space',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the To-Do List API');
@@ -25,6 +32,6 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -5,7 +5,7 @@ function Notification({ message, type, onClose }) {
   useEffect(() => {
     const timer = setTimeout(() => {
       onClose();
-    }, 3000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, [onClose]);
