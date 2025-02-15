c3c347c61b4f6d70b61b00527693cafabacb799c
itskundan-01 <2300030371@kluniversity.in>
2025-02-15 13:03:19 +0530

    removed unnecessary files

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || /**/'https://api.todo-app.kundanprojects.space'  || /**/ 'http://localhost:5000';
+  import.meta.env.VITE_API_BASE_URL || 'https://api.todo-app.kundanprojects.space' ;
 
 export const PUBLIC_URL =
-    import.meta.env.PUBLIC_URL || /**/'https://api.todo-app.kundanprojects.space'  || /**/ 'http://localhost:5000';
+    import.meta.env.PUBLIC_URL || 'https://api.todo-app.kundanprojects.space'  ;
 
+//|| 'http://localhost:5000'
