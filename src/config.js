8f482006d01555110e73c4dcf8861af01e28e096
itskundan-01 <2300030371@kluniversity.in>
2025-02-17 17:03:20 +0530

    Added some change

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ export const API_BASE_URL =
 
 export const PUBLIC_URL =
     import.meta.env.PUBLIC_URL || 'https://api.todo-app.kundanprojects.space'  ;
-export const ONE_SIGNAL_APP_ID ='feebfa31-41e0-4455-be8e-1a8fb956fd2c' ;
+export const OneSignalAppId ='feebfa31-41e0-4455-be8e-1a8fb956fd2c' ;
 //|| 'http://localhost:5000'
